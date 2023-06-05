1736582ea66756a07089563bdbe44686e4bf2c12
unknown <8268363@gmail.com>
2023-06-05 07:49:34 +0300

    change port to env

--- dist/src/main.js
+++ dist/src/main.js
@@ -2,9 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+require('dotenv').config();
 async function bootstrap() {
+    var _a;
+    const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000;
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(3000);
+    await app.listen(PORT);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
