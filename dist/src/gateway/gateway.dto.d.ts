ce596161f78a56e7b04c9b915db469cab024b603
unknown <8268363@gmail.com>
2023-06-05 08:07:22 +0300

    add client

--- dist/src/gateway/gateway.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class JoinDto {
-    roomCode: string;
-}
-type RtcDataType = 'description' | 'icecandidate';
-export declare class RtcDataDto {
-    type: RtcDataType;
-    data: RTCSessionDescriptionInit | RTCIceCandidate;
-}
-export {};
